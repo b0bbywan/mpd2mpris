88663bdb06d8e99363cd63faf7381a5d47f97cfe
Mathieu Réquillart <mathieu.requillart@gmail.com>
2026-06-07 00:54:21 +0200

    drop: i18n (babel + gettext)

--- shell.nix
+++ shell.nix
@@ -3,11 +3,9 @@ let
 in
   pkgs.mkShell {
     buildInputs = with pkgs; [
-      gettext
       (python311.withPackages (ps: with ps; [
         mpd2
         dbus-fast
-        babel
         pytest
         pytest-asyncio
         mypy
